e677aa06d852fddb6675d2f9b0af796ffb65e7bc
karolykele <karoly.kele@gmail.com>
2015-06-08 14:59:40 +0200

    Test commit 2

--- src/org/pestrada/android_tdd_playground/MainActivity.java
+++ src/org/pestrada/android_tdd_playground/MainActivity.java
@@ -6,8 +6,6 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends FragmentActivity {
-	
-	private static Bundle bundle;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
